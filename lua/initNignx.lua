c85e77a5fb6000328513f3f35edc21c0f2a3609a
erdaoya <zhaozhou2010jz@163.com>
2016-12-01 13:53:54 -0500

    redis subcribe

--- lua/initNignx.lua
+++ lua/initNignx.lua
@@ -1,8 +1,7 @@
 local services = ngx.shared.services;
 local redis = require ("redis_iresty")
 local red = redis:new()
---
---
+
 local set_shared = function()
     local ok, err = red:get("/user/user")
     if not ok then
@@ -14,6 +13,26 @@ local set_shared = function()
         ngx.log(ngx.ERR,"failed to set upstream",err)
     end
 end
+
+-- redis subscribe
+local subscrible_redis = function()
+	local red = redis:new({timeout=1000})
+	local func = red:subscribe( "service" )
+	if not func then
+		ngx.log(ngx.ERR, "subcrible failed ", err)
+	end
+	while true do
+		local res, err = func()
+		if err then
+			func(false)
+		end
+		if res then
+			ngx.log(ngx.ERR, "the message is ", res[3])
+		end
+	end
+end
+
+-- ngnix status judge
 local nginx_status = false
 ngx_sta_handler = function(premature)
     if premature then
@@ -26,11 +45,13 @@ ngx_sta_handler = function(premature)
     ngx.log(ngx.ERR, "nginx worker " .. ngx.worker.id() .." is runing...")
     ngx.timer.at(60, ngx_sta_handler)
 end
---ngx.timer.at(0, ngx_sta_handler)
+
+-- ngx.timer.at(0, ngx_sta_handler)
 if 0 == ngx.worker.id() then
     local ok, err = ngx.timer.at(0, ngx_sta_handler)
     if not ok then
         log(ngx.ERR, "failed to create timer: ", err)
         return
     end
+	local ok, err = ngx.timer.at(0, subscrible_redis)
 end
